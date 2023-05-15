53352971d42969e69cebac40fa2a2b242468a738
selestino-k <sandikroon23@gmail.com>
2023-05-16 04:46:53 +0800

    digocean db connect attempt

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize';
 
-const db = new Sequelize('topup_game','masbro','masbro-storekpg',{
+const db = new Sequelize('topup_game','admin','e5c09b96cd65fce2aca48d2d1ac8328f0cfcb26a056926fa',{
     host : '209.97.172.239',
     dialect : 'mysql',
 });
