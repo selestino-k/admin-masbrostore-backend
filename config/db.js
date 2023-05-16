17c49fadac2ca4eef4d71b6eae7ac7b56fa74b7e
selestino-k <sandikroon23@gmail.com>
2023-05-16 13:51:17 +0800

    DO DB cluster connection attempt

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 import {Sequelize} from 'sequelize';
 
 
-// const db = new Sequelize('topup_game','masbro','AVNS_GoK3DC9d-Jnprw2Djgr',{
-//     host : 'db-mysql-sgp1-11782-do-user-14103014-0.b.db.ondigitalocean.com:25060',
-//     dialect : 'mysql',
-// });
-
-const db = new Sequelize('topup_game',process.env.username,process.env.password,{
-    host : process.env.host,
+const db = new Sequelize('topup_game','masbro','AVNS_GoK3DC9d-Jnprw2Djgr',{
+    host : 'db-mysql-sgp1-11782-do-user-14103014-0.b.db.ondigitalocean.com',
+    port : '25060',
     dialect : 'mysql',
-    ssl : process.env.sslmode
 });
+
+// const db = new Sequelize('topup_game','masbro','masbro-storekpg',{
+//     host : '209.97.172.239',
+//     port :'3306',
+//     dialect : 'mysql'
+// });
 
 // const db = new Sequelize('topup_game','root','',{
 //     host : 'localhost',
