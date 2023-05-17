c77006ae5889372abf9d42b36105c60f11af831a
selestino-k <sandikroon23@gmail.com>
2023-05-17 13:58:56 +0800

    capek

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import userorderRoute from './routes/userorderRoute.js';
 import gameslistRoute from './routes/gameslistRoute.js';
-import { DELETE } from "sequelize/types/query-types.js";
 
 
 const app = express();
@@ -15,9 +14,7 @@ app.use(gameslistRoute);
 app.listen(5000, ()=> console.log("Server running on port 5000") );
 
 app.all('/', (req, res) => {
-    res.header('Access-Control-Allow-Origin: *');
-    res.header('Access-Control-Allow-Methods: POST,GET,OPTIONS, PATCH, DELETE ');
-
+ 
     console.log("Tes request sukses!")
     res.send('Tes API sukses')
 })
